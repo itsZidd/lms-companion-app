a3f924c1010c2db732bc951df4cbab6a8591d73f
itsZidd <zidanmalik02@gmail.com>
2025-06-25 05:34:23 +0700

    feat: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
 import localFont from "next/font/local";
 import "./globals.css";
 
 const myFont = localFont({
   src: "./fonts/OpenDyslexic-Regular.woff2",
-})
+});
 
 export const metadata: Metadata = {
   title: "LMS Companion",
@@ -17,11 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={myFont.className}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={myFont.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* The ThemeProvider is used to manage the theme of the application */}
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div><p>LMS Companion</p></div>
-  )
-}
-
-export default page
